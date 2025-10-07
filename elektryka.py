15e59fe99169597d66a1fa520f2d6d6a484f8fd3
edwinkarolczyk <edwinkarolczyk@gmail.com>
2025-10-07 20:04:53 +0200

    Fix cross-room link rendering collision

--- elektryka.py
+++ elektryka.py
@@ -639,7 +639,8 @@ class ElektrykaApp:
             if only and pick and (link.circuit_id != pick): continue
             a = idx[link.a_id]
             color = self._circuit_color_hex(link.circuit_id)
-            if link.b_id in idx:
+            is_local = (link.b_id in idx) and (not link.b_room or link.b_room == room.name)
+            if is_local:
                 b = idx[link.b_id]
                 self.canvas.create_line(a.x, a.y, b.x, b.y, fill=color, width=3, arrow="last")
             else:
